87f9f3f8a6e89735b62ecf5c69e331e514479f95
BrookeYang <brookeyang@tencent.com>
2019-07-30 17:23:34 +0800

    modify port
    Signed-off-by: BrookeYang <brookeyang@tencent.com>

--- hello-world.go
+++ hello-world.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
     http.HandleFunc("/", HelloServer)
-    http.ListenAndServe(":6000", nil)
+    http.ListenAndServe(":5000", nil)
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
